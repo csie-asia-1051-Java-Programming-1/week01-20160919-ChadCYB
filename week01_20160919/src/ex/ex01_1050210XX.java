d43730894786c21b4486b5a439ec92df0ae74bdd
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-09-19 15:43:17 +0800

    ex01-1 繳交

--- week01_20160919/src/ex/ex01_1050210XX.java
+++ week01_20160919/src/ex/ex01_1050210XX.java
@@ -2,14 +2,12 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入「華氏」溫度，輸出「攝氏」溫度 (華氏 = 攝氏*(9/5)+32)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 1050210XX CYB
  */
 
 public class ex01_1050210XX {
-
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println( (new java.util.Scanner(System.in)).nextFloat()*(9f/5f)+32f);
 	}
-
 }
